aa15ff02c0cd592fdddab60a07ad47a6bad2a457
ramamimu <ahm.f.ramadhani@gmail.com>
2024-05-28 15:17:48 +0700

    feat: chat service

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -1,12 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  User? getCurrentUser() {
+    return _auth.currentUser;
+  }
 
   Future<UserCredential> signInWithEmailPassword(String email, password) async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
+
+      // save user info if it doesn't exist
+      _firestore.collection("users").doc(userCredential.user!.uid).set(
+        {
+          "uid": userCredential.user!.uid,
+          'email': email
+        }
+      );
 
       return userCredential;
     } on FirebaseAuthException catch (e) {
@@ -18,6 +33,16 @@ class AuthService {
     try {
       UserCredential userCredential = await _auth
           .createUserWithEmailAndPassword(email: email, password: password);
+
+      // Update the user's display name
+      // await userCredential.user?.updateProfile(displayName: displayName);
+
+      // Optionally, you might want to reload the user to get the updated information
+      // await userCredential.user?.reload();
+
+      // Get the updated user
+      // User? updatedUser = FirebaseAuth.instance.currentUser;
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
