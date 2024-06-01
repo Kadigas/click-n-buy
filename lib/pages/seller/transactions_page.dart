361aaebe842147821ffd27a2561bcc77be8f8b6a
Andhika Bagas <87473932+Kadigas@users.noreply.github.com>
2024-06-01 23:12:32 +0700

    feat: scaffold bottom navbar

---
+++ lib/pages/seller/transactions_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SellerTransactionsPage extends StatelessWidget {
+  const SellerTransactionsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
