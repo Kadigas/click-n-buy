8b0d891f1a7141b848477f0b0d52ad00db5b62c8
Andhika Bagas <87473932+Kadigas@users.noreply.github.com>
2024-06-02 17:06:37 +0700

    feat: delete product

---
+++ lib/pages/seller/show_product.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fp_ppb/components/my_button.dart';
+import 'package:fp_ppb/pages/seller/edit_product.dart';
+import 'package:fp_ppb/service/product_service.dart';
+import 'package:intl/intl.dart';
+
+class ShowProductPage extends StatefulWidget {
+  final String productID;
+
+  const ShowProductPage({super.key, required this.productID});
+
+  @override
+  State<ShowProductPage> createState() => _ShowProductPageState();
+}
+
+class _ShowProductPageState extends State<ShowProductPage> {
+  late Future<DocumentSnapshot> _document;
+  final ProductService productService = ProductService();
+  final formatCurrency = NumberFormat.currency(locale: 'id_ID', symbol: 'Rp');
+
+  @override
+  void initState() {
+    super.initState();
+    _document = FirebaseFirestore.instance
+        .collection('products')
+        .doc(widget.productID)
+        .get();
+  }
+
+  void deleteProduct(String productID) {
+    productService.deleteProduct(productID);
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: FutureBuilder<DocumentSnapshot>(
+        future: _document,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          if (!snapshot.hasData || !snapshot.data!.exists) {
+            return const Center(child: Text('Document not found'));
+          }
+
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+          String productName = data['productName'];
+          String productDescription = data['productDescription'];
+          String productCategory = data['productCategory'];
+          String productPrice = formatCurrency.format(data['productPrice']);
+          int productStock = data['productStock'];
+          String productCondition = data['productCondition'];
+
+          return SafeArea(
+            child: SingleChildScrollView(
+              child: Container(
+                decoration: const BoxDecoration(color: Colors.white),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Center(
+                        child: Icon(
+                          Icons.keyboard,
+                          size: 250,
+                        ),
+                      ),
+                      const Divider(
+                        thickness: 1.0,
+                        color: Colors.grey,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        productPrice,
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(productName),
+                      const SizedBox(height: 10),
+                      const Divider(
+                        thickness: 1.0,
+                        color: Colors.grey,
+                      ),
+                      const Text(
+                        'Product Details',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      Text('Category: $productCategory'),
+                      const SizedBox(height: 10),
+                      Text('Condition: $productCondition'),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Divider(
+                        thickness: 1.0,
+                        color: Colors.grey,
+                      ),
+                      const Text(
+                        'Product Description',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(productDescription),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          MyButton(
+                            msg: 'Edit Product',
+                            color: Colors.black,
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => EditProductPage(
+                                          productID: widget.productID)));
+                            },
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          MyButton(
+                            msg: 'Delete Product',
+                            color: Colors.red,
+                            onTap: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: Text('Confirmation'),
+                                    content: Text(
+                                        'Are you sure you want to proceed?'),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context)
+                                              .pop();
+                                        },
+                                        child: Text('Cancel'),
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context)
+                                              .pop(); // Dismiss the dialog
+                                          deleteProduct(widget.productID);
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(
+                                            SnackBar(
+                                                content:
+                                                    Text('Succeeded delete product!')),
+                                          );
+                                        },
+                                        child: Text('Confirm'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
