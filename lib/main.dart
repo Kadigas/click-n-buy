30d7a6bfbda38130e0657b7db76efd2bb3762f98
Andhika Bagas <87473932+Kadigas@users.noreply.github.com>
2024-06-02 15:46:34 +0700

    feat: bottom navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:fp_ppb/pages/auth_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:fp_ppb/pages/seller/startup_screen.dart';
 import 'firebase_options.dart';
 import 'package:fp_ppb/pages/chat_page.dart';
 
