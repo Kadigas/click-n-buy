a106b6e4c0573c149ca0a99d6dc8869fb94015db
ramamimu <ahm.f.ramadhani@gmail.com>
2024-06-18 19:16:40 +0700

    feat: edit and delete message in message list
    fix: chat with own store

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -15,21 +15,55 @@ class Message {
   final String userName;
   final String userId;
 
-  Message({required this.message, required this.senderId, required this.senderEmail, required this.receiverId, required this.timestamp, this.type, this.imageLink, this.isDelete, this.isEdit, this.storeId, this.isSeller, required this.userName, required this.userId});
+  Message({
+    required this.message,
+    required this.senderId,
+    required this.senderEmail,
+    required this.receiverId,
+    required this.timestamp,
+    this.type,
+    this.imageLink,
+    this.isDelete,
+    this.isEdit,
+    this.storeId,
+    this.isSeller,
+    required this.userName,
+    required this.userId,
+  });
+
+  // Factory constructor to create a Message instance from Firestore document snapshot
+  factory Message.fromDocumentSnapshot(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return Message(
+      message: data['message'],
+      senderId: data['senderId'],
+      senderEmail: data['senderEmail'],
+      receiverId: data['receiverId'],
+      timestamp: data['timestamp'],
+      type: data['type'],
+      imageLink: data['imageLink'],
+      isDelete: data['isDelete'],
+      isEdit: data['isEdit'],
+      storeId: data['storeId'],
+      isSeller: data['isSeller'],
+      userName: data['userName'],
+      userId: data['userId'],
+    );
+  }
 
   Map<String, dynamic> toMap() {
     return {
-      'storeId': storeId?? '',
+      'storeId': storeId ?? '',
       'senderId': senderId,
       'senderEmail': senderEmail,
       'receiverId': receiverId,
       'message': message,
       'timestamp': timestamp,
-      'type': type?? "message",
-      'imageLink': imageLink?? "",
-      'isDelete': isDelete?? false,
-      'isEdit': isEdit?? false,
-      'isSeller': isSeller?? false,
+      'type': type ?? "message",
+      'imageLink': imageLink ?? "",
+      'isDelete': isDelete ?? false,
+      'isEdit': isEdit ?? false,
+      'isSeller': isSeller ?? false,
       'userName': userName,
       'userId': userId,
     };
