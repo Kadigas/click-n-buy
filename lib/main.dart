498cbd6e334bc88fd12028ee2b5f13ff76a0c674
Andhika Bagas <87473932+Kadigas@users.noreply.github.com>
2024-06-18 23:44:16 +0700

    fix(fcm): remove fcm

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:fp_ppb/pages/auth_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:fp_ppb/pages/transaction/transaction_page.dart';
-import 'package:fp_ppb/service/firebase_messaging.dart';
 import 'firebase_options.dart';
-
-final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await FirebaseMessage().initNotifications();
   runApp(const MyApp());
 }
 
@@ -28,10 +23,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: AuthPage(),
-      navigatorKey: navigatorKey,
-      routes: {
-        '/transactions': (context) => const TransactionPage(),
-      },
     );
   }
 }
